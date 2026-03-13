53a353e71b3dfb5477079d5f16d33aafe6a38b8e
Nidia Ardila <116224096+nidiaardila@users.noreply.github.com>
2026-03-13 12:30:19 +0000

    feat:conceptos basicos de python, comentarios y variables.

---
+++ python-esencial-lk-learning/01_hello_world.py
@@ -0,0 +1,12 @@
+
+print("Hello, World!")
+#este es mi comentario
+print("Hello World Again")#otro comentario
+#print("No se imprime")
+
+#este es un comentario
+#multilinea
+
+"""este es otro comentario
+multilinea con triple comilla"""
+
---
+++ python-esencial-lk-learning/02-variables.py
@@ -0,0 +1,15 @@
+nombre = 'Moises' #string
+
+edad = 27 #numerico
+
+estatura = 2.07 #float decimal
+
+positivo = True #boolean
+negativo = False
+
+
+#imprimir tipo de dato de las variables
+print(type(nombre))
+print(type(edad))
+print(type(estatura))
+print(type(positivo))
