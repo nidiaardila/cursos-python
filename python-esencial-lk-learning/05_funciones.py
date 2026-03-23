6913d8546d93d0670266b7d9dea6fd250c2497cd
Nidia Ardila <116224096+nidiaardila@users.noreply.github.com>
2026-03-23 11:07:34 +0000

    feat: Funciones, retorno y documentacion

--- python-esencial-lk-learning/05_funciones.py
+++ python-esencial-lk-learning/05_funciones.py
@@ -12,3 +12,39 @@ def perimetro_cuadrado(lado, unidades):
 
 perimetro_cuadrado(25, "metros")
 
+
+#Retorno de valores de una funcion y Documentacion de la funcion
+def perimetro_cuadrado(lado):
+  """Calcular el perimetro de un cuadrado
+  
+  ESta funcion recibe el valor de un lado de un cuadrado y a partir de esto calcula  y retorna
+  su perimetro
+
+  Args:
+    lado (int): medida del lado del cuadrado
+
+  Returns:
+    perimetro (int): retorna el perimetro del cuadrado  
+  """
+  perimetro = lado * 4
+  return perimetro
+
+def area_cuadrado(lado):
+  area = lado * lado
+  return area
+
+perimetro = perimetro_cuadrado(lado=5)
+area = area_cuadrado(lado=5)
+
+print("Area: ", area, "Perimetro: ", perimetro)  
+
+#Funcion con retorno multiple, Asignar la misma funcion a dos variables, area y perimetro
+def calcular_cuadrado(lado):
+  perimetro = lado * 4
+  area = lado * lado
+  return area, perimetro
+
+area, perimetro = calcular_cuadrado(lado=5)
+
+print("Area: ", area, "Perimetro: ", perimetro)  
+
