6aa2e828ff66ee1832c7df9fab927437ca6833b3
Nidia Ardila <116224096+nidiaardila@users.noreply.github.com>
2026-03-23 11:23:10 +0000

    feat: Modulos, importar modulos y paquetes en python

---
+++ python-esencial-lk-learning/06_modulos_paquetes.py
@@ -0,0 +1,11 @@
+#import _datetime
+#hora_actual = _datetime.datetime.now()
+#print("Hora actual: ", hora_actual)
+
+#import _datetime as _dt
+#hora_actual = _dt.datetime.now()
+#print("Hora actual: ", hora_actual)
+
+from datetime import datetime
+hora_actual = datetime.now()
+print("Hora actual: ", hora_actual)
